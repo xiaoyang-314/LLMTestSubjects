4f6c7827bf65418bbf8ffd4d8bf91ab3c322f5cb
xiaoyang-314 <YangXiao314@outlook.com>
2024-06-15 20:15:54 +0200

    Update Cosine Similarity.py

--- Code/Cosine Similarity.py
+++ Code/Cosine Similarity.py
@@ -1,1 +1,25 @@
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+from sentence_transformers.quantization import quantize_embeddings
 
+# 1. Modell und Tokenizer laden
+model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
+
+# Beispielabfrage und Dokumente
+query = 'Represent this sentence for searching relevant passages: A man is slicing potatoes'
+docs = [
+    query,
+    "A woman is peeling potato.",
+    "A woman is slicing an onion.",
+    "A woman is cutting a potato.",
+]
+
+# 2. Encode
+embeddings = model.encode(docs)
+
+# Optional: Quantize the embeddings
+binary_embeddings = quantize_embeddings(embeddings, precision="ubinary")
+
+# Ähnlichkeiten berechnen
+similarities = cos_sim(embeddings[0], embeddings[1:])
+print('similarities:', similarities)
